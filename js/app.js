9a4c4f5cb7c669dfc3487cb227c78806b0f77006
fnlCtrl <fnlctrl@gmail.com>
2015-03-12 15:42:05 +0800

    checkpoint

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
 (function () {
-	var app = angular.module('portfolio', ['ngRoute','ngAnimate'])
+	var app = angular.module('portfolio', ['ngRoute', 'ngAnimate'])
 		.config(['$routeProvider', function ($routeProvider) {
 			$routeProvider
 				.when('/:view', {
-					templateUrl: function(urlVar) {
+					templateUrl: function (urlVar) {
 						return 'html/' + urlVar.view + '.html'
 					},
 					controller: 'mainCtrl'
@@ -13,17 +13,28 @@
 				});
 		}])
 		.controller('mainCtrl', function ($scope) {
-
 		})
-		.controller('navCtrl', function ($scope, $http, $location) {
+		.controller('navCtrl', function ($http, $location) {
+			var self = this;
 			$http.get('data/data.json').success(function (data) {
-				$scope.views = data.views;
+				self.views = data.views;
 			});
-			$scope.checkActive = function(path) {
-				if ($location.path().substr(1,path.length) == path) {
+			self.checkActive = function (path) {
+				if ($location.path().substr(1, path.length) == path) {
 					return 'nav-dot-active'
 				} else {
 					return ''
+				}
+			}
+		})
+		.controller('sidebarCtrl', function () {
+			var self = this;
+			self.state = ''
+			self.toggle = function () {
+				if (self.state == '') {
+					self.state = 'sidebar-active'
+				} else {
+					self.state = ''
 				}
 			}
 		})
