9d90456314d714adb48697eba98ee05eb6b7179a
fnlctrl <fnlctrl@gmail.com>
2015-03-13 18:15:32 +0800

    checkpoint

--- js/app.js
+++ js/app.js
@@ -12,13 +12,16 @@
 					redirectTo: '/designs'
 				});
 		}])
-		.controller('mainCtrl', function ($scope) {
-		})
-		.controller('navCtrl', function ($http, $location) {
+		.controller('mainCtrl', function ($scope,$http) {
 			var self = this;
 			$http.get('data/data.json').success(function (data) {
 				self.views = data.views;
+				self.contacts = data.contacts;
+				self.skills = data.skills
 			});
+		})
+		.controller('navCtrl', function ($http, $location) {
+			var self = this;
 			self.checkActive = function (path) {
 				if ($location.path().substr(1, path.length) == path) {
 					return 'nav-dot-active'
@@ -27,15 +30,24 @@
 				}
 			}
 		})
-		.controller('sidebarCtrl', function () {
+		.controller('sidebarCtrl', function ($http) {
 			var self = this;
-			self.state = ''
+			self.state = '';
 			self.toggle = function () {
 				if (self.state == '') {
 					self.state = 'sidebar-active'
 				} else {
 					self.state = ''
 				}
-			}
+			};
+			$http.get('img/icon-star.svg').success(function(data){
+				self.svgText = data.replace('<!DOCTYPE svg>','').replace(/\"/g,"'").replace(/\r?\n|\r/g,'');
+				self.getSvgBg = function(color) {
+					self.svgText = self.svgText.replace('#FFFFFF',color);
+					console.log("data:image/svg+xml;utf8,"+window.btoa(self.svgText));
+					return 'url("data:image/svg+xml;utf8,'+self.svgText +'")';
+				};
+			});
+
 		})
 })();
