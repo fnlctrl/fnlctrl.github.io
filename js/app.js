6fc4f1be92a1f173e286fc31e1a2544cbff72535
fnlctrl <fnlctrl@gmail.com>
2015-03-14 10:41:19 +0800

    checkpoint

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 (function () {
 	var app = angular.module('portfolio', ['ngRoute', 'ngAnimate'])
-		.config(['$routeProvider', function ($routeProvider) {
+		.config(function ($routeProvider) {
 			$routeProvider
 				.when('/:view', {
 					templateUrl: function (urlVar) {
@@ -11,17 +11,33 @@
 				.otherwise({
 					redirectTo: '/designs'
 				});
-		}])
-		.controller('mainCtrl', function ($scope,$http) {
-			var self = this;
-			$http.get('data/data.json').success(function (data) {
-				self.views = data.views;
-				self.contacts = data.contacts;
-				self.skills = data.skills
-			});
 		})
-		.controller('navCtrl', function ($http, $location) {
+		.factory('dataService', function ($q, $http) {
+			var storage = {};
+			return {
+				getItems: function () {
+					var storageDefer = $q.defer();
+					if (Object.keys(storage).length > 0) {
+						storageDefer.resolve(storage);
+					}
+					else {
+						$http.get('data/data.json').success(function (data) {
+							storage = data;
+							storageDefer.resolve(data)
+						});
+					}
+					return storageDefer.promise;
+				}
+			}
+		})
+		.controller('mainCtrl', function () {
+			//not currently needed
+		})
+		.controller('navCtrl', function ($scope, $location, dataService) {
 			var self = this;
+			dataService.getItems().then(function (data) {
+				self.views = data['views'];
+			});
 			self.checkActive = function (path) {
 				if ($location.path().substr(1, path.length) == path) {
 					return 'nav-dot-active'
@@ -30,8 +46,40 @@
 				}
 			}
 		})
-		.controller('sidebarCtrl', function ($http) {
+		.controller('sidebarCtrl', function ($scope, dataService) {
 			var self = this;
+			var colors = [];
+			var shapes = [];
+			dataService.getItems().then(function (data) {
+				self.contacts = data['contacts'];
+				self.skills = data['skills'];
+				colors = self.skills.map(function (obj) {
+					return obj.style.color
+				});
+				shapes = self.skills.map(function (obj) {
+					return obj.style.shape
+				});
+			});
+			self.getSkillStyle = function () {
+				return colors.map(function(color){
+					return shapes.map(function(shape){
+						return [
+							'._',color,':hover .',shape,'{',
+								'background: #',color,
+							'}'
+						].join('');
+					}).append(
+						[
+							'._',color,':hover path {',
+							'fill: #',color,
+							'}'
+						].join('')
+					).join('\n')
+				});
+
+				//._{{skill.style.color}}:hover path { fill: #{{ skill.style.color }} }
+				//._{{skill.style.color}}:hover .{{skill.style.shape}} { background: #{{ skill.style.color }} }
+			};
 			self.state = '';
 			self.toggle = function () {
 				if (self.state == '') {
@@ -40,14 +88,5 @@
 					self.state = ''
 				}
 			};
-			$http.get('img/icon-star.svg').success(function(data){
-				self.svgText = data.replace('<!DOCTYPE svg>','').replace(/\"/g,"'").replace(/\r?\n|\r/g,'');
-				self.getSvgBg = function(color) {
-					self.svgText = self.svgText.replace('#FFFFFF',color);
-					console.log("data:image/svg+xml;utf8,"+window.btoa(self.svgText));
-					return 'url("data:image/svg+xml;utf8,'+self.svgText +'")';
-				};
-			});
-
 		})
 })();
