316c75c1b3483e767be511313c6e14abee0db7a3
宋铄运 <shuoyun.ssy@alibaba-inc.com>
2015-08-25 16:53:01 +0800

    modify webpack config, various changes

--- webpack.config.js
+++ webpack.config.js
@@ -7,29 +7,27 @@ var APP = path.join(__dirname, 'app');
 module.exports = {
 	context: APP,
 	entry: {
-		app: ['webpack/hot/dev-server', './app.es6']
+		app: './app.es6'
 	},
 	output: {
-		path: './',
-		filename: 'bundle.js'
+		path: './assets',
+		filename: 'bundle.js',
+		publicPath: ''
 	},
 	module: {
 		loaders: [{
 			test: /\.es6$/,
 			exclude: /(node_modules|bower_components)/,
-			loaders: ['uglify', 'ng-annotate', 'babel-loader']
+			loader: 'ng-annotate!babel'
 		}, {
 			test: /\.css$/,
-			loader: "style!css!autoprefixer"
+			loader: "style!raw!autoprefixer"
 		}, {
-			test: /\.(html|svg)$/,
-			loader: "html"
+			test: /\.html$/,
+			loader: "raw"
 		}, {
 			test: /\.json$/,
 			loader: 'json'
-		}, {
-			test: /\.jpe?g$|\.gif$|\.png$|\.svg$|\.woff$|\.ttf$|\.wav$|\.mp3$/,
-			loader: "file"
 		}]
 	},
 	resolve: {
